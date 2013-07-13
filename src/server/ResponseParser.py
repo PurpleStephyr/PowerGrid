85048fce097a6d82f8b77cff8c4c4fe40d8d35bd
Nikolai Semenenko <nsemenen@uwaterloo.ca>
2013-07-13 15:08:10 -0400

    Added supplyPowerForCities and cityPurchase in ResponseParser

--- src/server/ResponseParser.py
+++ src/server/ResponseParser.py
@@ -1,6 +1,6 @@
 # File ResponseParser.py
 # This file is a part of PowerGrid
-# Copyright 2013 Steph Kraemer
+# Copyright 2013 Steph Kraemer, Nikolai Semenenko
 
 # PowerGrid is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with PowerGrid.  If not, see <http://www.gnu.org/licenses/>.
 
-import re #regex
+import re as regex
 
 from src.ServerRequestTypes import *
 
@@ -25,23 +25,32 @@ class ResponseParser:
 
     def parse(response):
         try:
-            splitResponse = re.split('\n', response)
+            splitResponse = regex.split('\n', response)
             requestId = splitResponse[0]
             requestType = processHandler.getRequestType(requestId)
-            if int(requestType) == ServerRequestTypes.AUCTION_START:
+            if int(requestType) == ServerRequestTypes.AuctionStart:
                 self.processHandler.writeResponse(int(splitResponse[1]))
-            elif int(requestType) == ServerRequestTypes.POWER_PLANT_BID:
+            elif int(requestType) == ServerRequestTypes.PowerPlantBid:
                 self.processHandler.writeResponse(int(splitResponse[1]))
-            elif int(requestType) == ServerRequestTypes.RESOURCE_PURCHASE:
+            elif int(requestType) == ServerRequestTypes.ResourcePurchase:
                 parsedResponse = []
                 for line in splitResponse[1:]:
-                    resourcePair = re.split('\\s+', line)
+                    resourcePair = regex.split('\\s+', line)
                     parsedResponse.append((resourcePair[0], int(resourcePair[1])))
                 self.processHandler.writeResponse(parsedResponse)
-            elif int(requestType) == ServerRequestTypes.CITY_PURCHASE:
-                self.processHandler.writeResponse(None)
-            elif int(requestType) == ServerRequestTypes.SUPPLY_POWER_FOR_CITIES:
-                self.processHandler.writeResponse(None)
+            elif int(requestType) == ServerRequestTypes.CityPurchase:
+                parsedResponse = []
+                for line in splitResponse[1:]:
+                    cityList = regex.split('\\s+', line)
+                    parsedResponse.append(cityList)
+                self.processHandler.writeResponse(parsedResponse)
+            elif int(requestType) == ServerRequestTypes.SupplyPowerForCities:
+                # expects first line is number of cities - the rest are power plant numbers
+                parsedPowerPlants[]
+                for line in splitResponse[2:]:
+                     parsedPowerPlants.append(line)
+                parsedResponse = (int(splitResponse[1]), parsedPowerPlants)           
+                self.processHandler.writeResponse(parsedResponse)
             else:
                 self.processHandler.writeResponse(None)
         except:
